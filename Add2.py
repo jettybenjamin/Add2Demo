9bd9287179cf52b71acef0608853cfca696030fe
jettybenjamin <jettybenjamin@amaljyothi.ac.in>
2024-04-03 15:24:50 +0530

    Second

--- Add2.py
+++ Add2.py
@@ -2,9 +2,10 @@ number1 = 10
 number2 = 20
 number3 = 30
 number4=30
+number5=50
 
 # Add the numbers
-sum = number1 + number2 + number3+number4
+sum = number1 + number2 + number3+number4 + number5
 
 # Display the result
 print("Sum:", sum)
